49e3e76207452788b2dc090eea656de8a6ed8dfa
Ameesha <ameeshaagrawal12@gmail.com>
2022-02-13 22:38:19 +0530

    Added package.json and hardhat config

---
+++ hardhat.config.js
@@ -0,0 +1,100 @@
+require("dotenv").config();
+
+require("solidity-coverage");
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+require("@nomiclabs/hardhat-solhint");
+require("hardhat-deploy");
+require("hardhat-deploy-ethers");
+require("hardhat-gas-reporter");
+require("hardhat-watcher");
+
+const accounts = [process.env.DEPLOYER_PRIVATE_KEY];
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  abiExporter: {
+    path: "./abi",
+    clear: false,
+    flat: true,
+  },
+  defaultNetwork: "hardhat",
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+  gasReporter: {
+    coinmarketcap: process.env.COINMARKETCAP_API_KEY,
+    currency: "USD",
+    enabled: process.env.REPORT_GAS === "true",
+    excludeContracts: ["contracts/mocks/", "contracts/libraries/"],
+  },
+  mocha: {
+    timeout: 20000,
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.4",
+        optimizer: {
+          enabled: true,
+          runs: 200,
+        },
+      },
+    ],
+  },
+  paths: {
+    artifacts: "artifacts",
+    cache: "cache",
+    deploy: "deploy",
+    deployments: "deployments",
+    imports: "imports",
+    sources: "contracts",
+    tests: "test",
+  },
+  networks: {
+    hardhat: {
+      saveDeployments: true,
+      tags: ["test", "local"],
+      allowUnlimitedContractSize: true,
+      accounts: {
+        accountsBalance: "10000000000000000000000000",
+      },
+    },
+    ropsten: {
+      url: `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts,
+      chainId: 3,
+      live: true,
+      saveDeployments: true,
+      tags: ["staging"],
+      gasPrice: 5000000000,
+      gasMultiplier: 2,
+    },
+  },
+  tenderly: {
+    project: process.env.TENDERLY_PROJECT,
+    username: process.env.TENDERLY_USERNAME,
+  },
+  watcher: {
+    compile: {
+      tasks: ["compile"],
+      files: ["./contracts"],
+      verbose: true,
+    },
+  },
+};
